117fca9bda5bc84f88dfa1f0706fc900f6ba1c8c
oopssanboy <oopssanboy@gmail.com>
2026-04-02 01:11:51 +0700

    test payment momo v6

--- app/controllers/User/CheckoutController.php
+++ app/controllers/User/CheckoutController.php
@@ -149,11 +149,7 @@ $ipnUrl = trim($ipnUrl);
         curl_close($ch);
 
         $jsonResult = json_decode($result, true);
-        echo "<pre>";
-print_r($jsonResult);
-
-echo $rawHash;
-exit;
+        
 
         // 6. KIỂM TRA PHẢN HỒI, NẾU ĐÚNG THÌ CHUYỂN TRANG
         if (isset($jsonResult['payUrl'])) {
